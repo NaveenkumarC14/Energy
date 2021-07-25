5a6ed9bc53d58f33c771ef5a8502fbd31eb9b702
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-07-26 02:04:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,8 +32,6 @@ re_capacity_date = str(res.json()["re_capacity_date"]) + ""
 
 st.markdown('''
 
-<div class="jumbotron text-center" style='padding: 0px';background-color:#fff>
- <div class="row" style="background-color:#fff;width:100%;margin:auto;">
 <div 
       <div class="row-sm-4">
       <p style='text-align:center;  font-size: 15px; color: green'>Solar</p>
@@ -56,8 +54,6 @@ st.markdown('''
      </div>
      </div>
  
-</div>
 
-</div>
 
  ''', unsafe_allow_html=True);
