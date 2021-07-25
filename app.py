9ef322f763b8c6c492189d84fde83624380e62d1
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-07-25 23:18:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ str(state_code)
 import requests as rq
 url = "https://naveenkumarc_14.fred.sensetecnic.com/api/GetStateEnergy"
 headers = { "Authorization" : "Basic bmF2ZWVua3VtYXJjXzE0Ok5hdmVlbkA2Ng==",
-"Content-Type" : "application/json" }
+		"Content-Type" : "application/json",
+	  	"Accept" : "application/json" }
 post_body = {
 	"StateCode" : state_code
 }
