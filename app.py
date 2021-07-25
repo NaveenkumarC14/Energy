06d39058f772aa700bfc572eb04b7ca94ed07af0
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-07-25 23:16:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ df=pd.read_csv('States_RE.csv')
 
 state=st.sidebar.selectbox('Select a state',df['StateName'].unique())
 state=df[df['StateName']==state]
-state_code=a['StateCode'].iloc[0]
+state_code=state['StateCode'].iloc[0]
 str(state_code)
 
 import requests as rq
@@ -12,7 +12,7 @@ url = "https://naveenkumarc_14.fred.sensetecnic.com/api/GetStateEnergy"
 headers = { "Authorization" : "Basic bmF2ZWVua3VtYXJjXzE0Ok5hdmVlbkA2Ng==",
 "Content-Type" : "application/json" }
 post_body = {
-	"StateCode" : 1
+	"StateCode" : state_code
 }
 res = rq.post(url=url,data = post_body , headers = headers)
 
