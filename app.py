475fb05b07ecdb98956c86734e3b5e02afbcaa0a
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-07-26 02:09:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ re_capacity = str(res.json()["re_capacity"]) + "MW"
 re_capacity_date = str(res.json()["re_capacity_date"]) + ""
 
 
-col1,col2 = st.beta_colimns([3,1])
+col1,col2 = st.beta_columns([3,1])
