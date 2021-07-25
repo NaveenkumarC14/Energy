0cafdecc4624cf015eb25dd6883123654e899e89
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-07-26 01:16:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,21 +44,24 @@ st.markdown('''
       <p style='text-align: center; font-size: 15px; color: black'></p>
       <p style='text-align: center; font-size: 40px; font-weight: 600; color: black'>''' + str(solar_gen) + '''</p>
       <p style='text-align:center; font-weight: 400 ; color: black'>Solar Generation</p>
-      <p style='text-align:center; font-weight: 400 ; color: black'>''' + solar_gen_date + '''</p>
+      
+      <p style='text-align: center ; font-size: 40px; font-weight: 600; color: blue'>''' + solar_gen_date + '''</p>
     </div>    
     <div class="row-sm-3">    
       
       <p style='text-align: center; font-size: 15px; color: green'></p>
       <p style='text-align: center ; font-size: 40px; font-weight: 600; color: green'>''' + str(wind_gen) + '''</p>  
       <p style='text-align: center; background-color: #fff; font-weight: 400 ;color: green'>Wind Generation</p>
-      <p style='text-align:center; font-weight: 400 ; color: black'>''' + solar_gen_date +'''</p>
+      
+      <p style='text-align: center ; font-size: 40px; font-weight: 600; color: blue'>''' + solar_gen_date +'''</p>
      </div>     
     <div class="row-sm-3">    
       
       <p style='text-align: center; font-size: 15px; color: green'></p>
       <p style='text-align: center ; font-size: 40px; font-weight: 600; color: blue'>''' + str(re_capacity) + '''</p>    
       <p style='text-align: center; background-color: #fff; font-weight: 400 ;color: blue'>Installed RE Capacity</p>
-      <p style='text-align:center; font-weight: 400 ; color: black'>''' + solar_gen_date +'''</p>
+      
+      <p style='text-align: center ; font-size: 40px; font-weight: 600; color: blue'>''' + solar_gen_date +'''</p>
     </div> 
   </div>
 </div>
