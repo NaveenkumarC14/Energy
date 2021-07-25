6c5f4d31919c4ff9f81f61ceefd447ed132bffd9
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-07-25 23:14:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,13 @@ state=df[df['StateName']==state]
 state_code=a['StateCode'].iloc[0]
 str(state_code)
 
+import requests as rq
+url = "https://naveenkumarc_14.fred.sensetecnic.com/api/GetStateEnergy"
+headers = { "Authorization" : "Basic bmF2ZWVua3VtYXJjXzE0Ok5hdmVlbkA2Ng==",
+"Content-Type" : "application/json" }
+post_body = {
+	"StateCode" : 1
+}
+res = rq.post(url=url,data = post_body , headers = headers)
 
-url = 
+res.json()
