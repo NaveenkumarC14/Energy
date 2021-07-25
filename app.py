0bb57886ed05aed3dffb3adf93d88417a46f6cd7
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-07-25 22:56:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import streamlit as st
 df=pd.read_csv('States_RE.csv')
 
 state=st.sidebar.selectbox('Select a state',df['StateName'].unique())
-df[df['StateCode']==State]
+df[df['StateCode']==state]
