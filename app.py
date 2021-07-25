fd01bc1096323c0251cb8540776eb04ca60751b3
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-07-26 02:04:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,21 +35,21 @@ st.markdown('''
 <div class="jumbotron text-center" style='padding: 0px';background-color:#fff>
  <div class="row" style="background-color:#fff;width:100%;margin:auto;">
 <div 
-      <div class="row-sm-3">
+      <div class="row-sm-4">
       <p style='text-align:center;  font-size: 15px; color: green'>Solar</p>
       <p style='text-align: center; font-size: 15px; color: green'></p>
      <p style='text-align: center ; font-size: 42px; font-weight: bold; color: blue'>''' + str(CO2) +'''</p>
      </div>
       </div>
  <div 
-     <div class="row-sm-3">
+     <div class="row-sm-4">
       <p style='text-align:center;  font-size: 15px; color: green'>Solar</p>
       <p style='text-align: center; font-size: 15px; color: green'></p>
      <p style='text-align: center ; font-size: 42px; font-weight: bold; color: green'>''' + str(solar_gen) +'''</p>
       </div>
       </div>
    <div
-   <div class="row-sm-3"> 
+   <div class="row-sm-4"> 
       <p style='text-align:center;  font-size: 15px; color: green'>RE Capasity</p>
       <p style='text-align: center; font-size: 15px; color: green'></p>
      <p style='text-align: center ; font-size: 42px; font-weight: bold; color: red'>''' + str(re_capacity) +'''</p>
