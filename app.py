1695b5eaad39c78abd1af5a71680a84fc1968232
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-07-26 01:31:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ st.markdown('''
     
       <p style='text-align:center; font-weight: 400 ; color: black'>Solar</p>
       <p style='text-align: center; font-size: 15px; color: red'></p>
-      <p style='text-align: center; font-size: 40px; font-weight: 600; color: red'>''' + str(CO2) + '''</p>
+      <p style='text-align: center; font-size: 40px; font-weight: 600; color: red'>''' + str(CO2) '''</p>
     
   </div>
 </div>
