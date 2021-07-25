f8413d0bf83f7488c05bab48c5c1db9a8e104d92
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-07-25 22:46:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import pandas as pd
 import streamlit as st
 df=pd.read_csv('States_RE.csv')
-st.sidebar.selectbox('Select a state',df['State'].unique())
+st.sidebar.selectbox('Select a state',df['StateName'].unique())
