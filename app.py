e3df9578594c217e88f00ab4839938038cebd1c4
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-07-26 10:39:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -150,30 +150,7 @@ if select=='State':
 	''', unsafe_allow_html=True);
 
 if select=='India':
-	state=st.selectbox('Select a state',df['State'].unique())
-	state=df[df['State']==state]
-	state_code=state['StateCode'].iloc[0]
-	import requests as rq
-	#import ujson as json
-	url = "https://naveenkumarc_14.fred.sensetecnic.com/api/GetStateEnergy?st=" +str(state_code)
-	#r=rq.get("https://naveenkumarc_14.fred.sensetecnic.com/api/GetStateEnergy?st=1",auth = ("naveenkumarc_14", "Naveen@66"))
-	#r.status_code
-	headers = { 
-			"Content-Type" : "application/json",
-			"Accept" : "application/json" 
-		   }
-	post_body = {
-		"StateCode" : 1
-	}
-	res = rq.get(url, auth = ("naveenkumarc_14", "Naveen@66"))
-	stateName = str(res.json()["StateName"]) + ""
-	CO2 = str(res.json()["CO2"]) + ""
-	CO2_Date  = str(res.json()["CO2_Date"]) + ""
-	solar_gen = str(res.json()["solar_gen"]) + "MU"
-	solar_gen_date = str(res.json()["solar_gen_date"]) + ""
-	wind_gen = str(res.json()["wind_gen"]) + "MU"
-	re_capacity = str(res.json()["re_capacity"]) + "MW"
-	re_capacity_date = str(res.json()["re_capacity_date"]) + ""
+	
 
 	
 	st.markdown('''
@@ -187,6 +164,8 @@ if select=='India':
 				<p style='text-align: center; font-size: 40px; font-weight: 600; color:  blue'>487625 <span class="card-title-1">tCO<sub>2</sub></span></p></div>
 				<div 
 				<p style ='text-align: center; background-color: #d1f0a2; font-weight: 400 ;color:  blue'>CO2 emissions mitigated</p> </div>  
+				<div 
+				<p style ='text-align: center; background-color: white; font-weight: 400 ;color:  blue'>''' + str(daily_gen['Date'].iloc[-1]) + '''</p></div>
 			    <div 
 				<p style ='text-align: center; background-color: #d1f0a2; font-weight: 400 ;color: blue'></p>
 				</div>  
@@ -208,6 +187,8 @@ if select=='India':
 				<p style='text-align: center; font-size: 40px; font-weight: 600; color:  blue'>''' + str(daily_gen['Solar (MU)'].iloc[-1]) +  ''' MU</p></div>
 				<div 
 				<p style ='text-align: center; background-color: white; font-weight: 400 ;color:  blue'>Solar Generation</p> </div>   
+				<div 
+				<p style ='text-align: center; background-color: white; font-weight: 400 ;color:  blue'>''' + str(daily_gen['Date'].iloc[-1]) + '''</p></div>
 			    <div 
 				<p style ='text-align: center; background-color: white; font-weight: 400 ;color: blue'></p>
 				</div>  
@@ -228,7 +209,9 @@ if select=='India':
 	<div
 				<p style='text-align: center; font-size: 40px; font-weight: 600; color:  blue'>''' + str(daily_gen['Wind (MU)'].iloc[-1]) +  ''' MU</p></div>
 				<div 
-				<p style ='text-align: center; background-color: #d1f0a2; font-weight: 400 ;color:  blue'>Wind Generation</p> </div>   
+				<p style ='text-align: center; background-color: #d1f0a2; font-weight: 400 ;color:  blue'>Wind Generation</p> </div>  
+				<div 
+				<p style ='text-align: center; background-color: white; font-weight: 400 ;color:  blue'>''' + str(daily_gen['Date'].iloc[-1]) + '''</p></div>
 			    <div 
 				<p style ='text-align: center; background-color: white; font-weight: 400 ;color: blue'></p>
 				</div>  
@@ -250,6 +233,8 @@ if select=='India':
 				<p style='text-align: center; font-size: 40px; font-weight: 600; color:  blue'>''' + str(install_capa['All (MW)'].iloc[-1]) +  ''' mw</p></div>
 				<div 
 				<p style ='text-align: center; background-color: white; font-weight: 400 ;color:  blue'>Installed RE Capacity</p> </div>   
+				<div 
+				<p style ='text-align: center; background-color: white; font-weight: 400 ;color:  blue'>''' + str(install_capa['Month'].iloc[-1]) + '''</p></div>
 			    <div 
 				<p style ='text-align: center; background-color: white; font-weight: 400 ;color: blue'></p>
 				</div>  
