d50840798ce8af7f884aee72eec524c09d9a4460
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-07-26 12:41:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -176,6 +176,6 @@ if state1=='All':
 		 ''', unsafe_allow_html=True);
 	install_capa1=pd.melt(install_capa,id_vars = 'Month', var_name="Source", value_name="Value")	
 
-	fig = px.line(install_capa1, x="Month", y="Value", color="Source",line_group="Source", hover_name="Month", width=1000, height=500)
+	fig =px.bar(install_capa1,x='Month',y="Value",color='Source')
 	st.plotly_chart(fig)
 
