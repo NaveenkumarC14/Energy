1e438b747e506f35b7cced6a20aad8e4b4a6e97a
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-07-26 08:12:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ st.markdown('''
 			<p style ='text-align: center; background-color: #d1f0a2; font-weight: 400 ;color: blue'>''' + str(CO2_Date) + '''</p>
 			</div>
 			<div 
-			<p style ='text-align: center; background-color: white; font-weight: 400 ;color: blue'></p>
+			<p style ='text-align: center; background-color: white; font-weight: 400 ;color: blue'>   </p>
 			</div>  
                     </div>
                 </div>
