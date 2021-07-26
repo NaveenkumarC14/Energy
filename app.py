491946249803caf77b17d925848177b27949fc1d
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-07-26 10:57:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -246,7 +246,7 @@ if select=='India':
 	    </div>
 	''', unsafe_allow_html=True);
 	
-       daily_gen1=pd.melt(daily_gen,id_vars = 'Date',var_name="Source",value_name="Value")	
+       daily_gen1=pd.melt(daily_gen,id_vars = ['Date'] ,var_name="Source",value_name="Value")	
 
        fig = px.line(daily_gen1, x="Date", y="Value", color="Source",
               line_group="Source", hover_name="Source")
