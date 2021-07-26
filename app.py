e7dc68deb4c882f69948fc4679d6b0b3634f85c5
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-07-26 13:49:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -209,7 +209,7 @@ if state1=='All':
 	install_capa1=pd.melt(ut,id_vars = 'Month', var_name="Source", value_name="Value")	
         
 	
-	fig=px.bar(install_capa1,x='Month',y="Value",color='Source',barmode='group', width=1000, height=500)
+	fig=px.bar(install_capa1,x='Month',y="Value",color='Source',barmode='group',labels={'Value':'Energy (%)'}, width=1000, height=500)
 	st.plotly_chart(fig)
 	st.markdown('''
 		<div class="jumbotron text-center" style='background-color: #fff'>
