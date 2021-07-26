7c0ef4b58f18733fbfe167284b86c6584b2e350d
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-07-26 10:21:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -150,6 +150,34 @@ if select=='State':
 	''', unsafe_allow_html=True);
 
 if select=='India':
+	state=st.selectbox('Select a state',df['State'].unique())
+	state=df[df['State']==state]
+	state_code=state['StateCode'].iloc[0]
+#str(state_code)
+
+   
+			   
+	import requests as rq
+	#import ujson as json
+	url = "https://naveenkumarc_14.fred.sensetecnic.com/api/GetStateEnergy?st=" +str(state_code)
+	#r=rq.get("https://naveenkumarc_14.fred.sensetecnic.com/api/GetStateEnergy?st=1",auth = ("naveenkumarc_14", "Naveen@66"))
+	#r.status_code
+	headers = { 
+			"Content-Type" : "application/json",
+			"Accept" : "application/json" 
+		   }
+	post_body = {
+		"StateCode" : 1
+	}
+	res = rq.get(url, auth = ("naveenkumarc_14", "Naveen@66"))
+	stateName = str(res.json()["StateName"]) + ""
+	CO2 = str(res.json()["CO2"]) + ""
+	CO2_Date  = str(res.json()["CO2_Date"]) + ""
+	solar_gen = str(res.json()["solar_gen"]) + "MU"
+	solar_gen_date = str(res.json()["solar_gen_date"]) + ""
+	wind_gen = str(res.json()["wind_gen"]) + "MU"
+	re_capacity = str(res.json()["re_capacity"]) + "MW"
+	re_capacity_date = str(res.json()["re_capacity_date"]) + ""
 	
 	st.markdown('''
 
