b120b476f51f4fd8aaf9b2d09298e8a9599390a8
NaveenkumarC14 <76938207+NaveenkumarC14@users.noreply.github.com>
2021-08-10 04:28:59 +0100

    Update app.py

--- app.py
+++ app.py
@@ -240,7 +240,6 @@ st.markdown('''
 	<tr role="row" class="odd"><td>DailyRE08082021</td>
 	<td class="sorting_1">2021-08-08</td>
 	<td>
-	<td>
 	<a href="https://cea.nic.in/wp-content/uploads/daily_reports/8_Aug.xlsx" download=""><img src="https://cea.nic.in/wp-content/uploads/2020/05/excel.png" class="ml-5"></i></a></td></tr>
 	<tr role="row" class="even"><td>DailyRE07082021</td><td class="sorting_1">2021-08-07</td><td>
 	<a href="https://cea.nic.in/wp-content/uploads/daily_reports/7_Aug.pdf" download=""><img src="https://cea.nic.in/wp-content/uploads/2020/05/pdf.png"</i></a>
